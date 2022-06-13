7d6a6860222199319a6f86193666ee1739b142c2
oussema <37280264+oussemamw@users.noreply.github.com>
2022-06-12 20:08:49 -0700

    get info
    get data(title,desc..)
    add csv file

--- main.py
+++ main.py
@@ -3,21 +3,36 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 import random
+import csv
 
 driver = webdriver.Chrome()
 driver1 = webdriver.Chrome()
 
 driver.get('https://nilsolsson.se/sv/Produkter')
+table_info = ['title', 'price', 'description', 'image', 'sub_categ', 'categ']
 
 
-
+csv_data=[]
 def crawl(d):
+    data={}
     product_links=d.find_elements_by_class_name("itemnamelink")
     for product in product_links:
         link=product.get_attribute("href")
         driver1.get(link)
-
-
+        data['title']=driver1.find_element_by_class_name('title').text
+        data['price']=driver1.find_element_by_class_name('price').text
+        data['description']=driver1.find_element_by_class_name('description').text
+        data['image']=driver1.find_element_by_class_name('image_links').get_attribute("href")
+        list_categ=driver1.find_elements_by_class_name('breadlink')
+        if len(list_categ)>3:
+            data['sub_categ']=list_categ[-1].text
+            data['categ']=list_categ[-2].text
+        else:
+            data['sub_categ']=""
+            data['categ']=list_categ[-1].text
+        csv_data.append(data)
+        data={}
+    
 link_categ=[]
 all_categ=driver.find_elements_by_class_name("group-link")
 for categ in all_categ:
@@ -32,7 +47,7 @@ for l in link_categ:
         for sub_categ in all_sub_categ:
             link_sub_categ.append(sub_categ.get_attribute("href"))
 
-        print(link_sub_categ)
+        
 
     except:
         pass
@@ -47,6 +62,11 @@ for l in link_categ:
 
     else:
         crawl(driver)
+
+with open('data.csv', 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames = table_info)
+        writer.writeheader()
+        writer.writerows(csv_data)
 
         
 
